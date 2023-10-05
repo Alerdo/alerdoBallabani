8baf9cb998d1b730d3452f38977937646a65d0e0
Alerdo <alerdo23@gmail.com>
2023-10-05 20:47:55 +0100

    Create getNearbyCountries getNearbyCountries api calls, and edid php file

--- project1/geonamesExample/geonamesExample/libs/js/script.js
+++ project1/geonamesExample/geonamesExample/libs/js/script.js
@@ -1,19 +1,23 @@
 let countryInfo = null;
 
 $(document).ready(function() {
-
     $('#btnRun').click(function() {
         const country = $('#selCountry').val();
         const language = $('#selLanguage').val();
         
         // Make the initial API call on "Run" button click
         makeAPICall('getCountryInfo', { country, lang: language }, function(result) {
+            // Add latitude and longitude to the response data
+            result.data[0].latitude = parseFloat(result.data[0].north) + parseFloat(result.data[0].south) / 2;
+            result.data[0].longitude = parseFloat(result.data[0].east) + parseFloat(result.data[0].west) / 2;
+    
             countryInfo = result;
             console.log("Initial Country Info:", countryInfo);
             // Optionally, you can display the result here or anywhere you need
             $('#apiResults').html(JSON.stringify(result.data));
         });
     });
+    
 
     $('button[data-api-id="api1"]').click(function() {
         if (countryInfo) {
@@ -21,15 +25,24 @@ $(document).ready(function() {
         }
     });
 
+    
+
     $('button[data-api-id="api2"]').click(function() {
         if (countryInfo) {
+            const country = $('#selCountry').val(); // Get the selected country from the HTML
+    
             const params = {
-                lat: countryInfo.data[0].latitude,
-                lng: countryInfo.data[0].longitude
+                country: country, // Use the selected country for nearby countries
+                username: 'AlerdoBallabani' // Replace with your actual username
             };
-            makeAPICall('getWeatherInfo', params);
+    
+            console.log("Nearby Countries Params:", params); // Debugging statement
+    
+            makeAPICall('getNearbyCountries', params);
         }
     });
+    
+    
 
     $('button[data-api-id="api3"]').click(function() {
         if (countryInfo) {
@@ -40,9 +53,13 @@ $(document).ready(function() {
                 west: countryInfo.data[0].west,
                 lang: $('#selLanguage').val()
             };
+    
+            console.log("Cities Info Params:", params); // Debugging statement
+    
             makeAPICall('getCitiesInfo', params);
         }
     });
+    
 
     function makeAPICall(action, params, callback = null) {
         params.action = action;
--- project1/geonamesExample/geonamesExample/libs/php/getCountryInfo.php
+++ project1/geonamesExample/geonamesExample/libs/php/getCountryInfo.php
@@ -1,33 +1,48 @@
 <?php
 
-	// remove for production
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+$executionStartTime = microtime(true);
+$username = "AlerdoBallabani"; 
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+$action = $_REQUEST['action'];
 
-	$executionStartTime = microtime(true);
+switch ($action) {
+    case 'getCountryInfo':
+        $url = "http://api.geonames.org/countryInfoJSON?formatted=true&lang=" . $_REQUEST['lang'] . "&country=" . $_REQUEST['country'] . "&username=" . $username . "&style=full";
+        break;
 
-	$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=flightltd&style=full';
+        case 'getNearbyCountries':
+            // Include the country code and username in the URL
+            $url = "http://api.geonames.org/neighboursJSON?country=" . $_REQUEST['country'] . "&username=" . $username;
+            break;
+        
+    case 'getCitiesInfo':
+        $url = "http://api.geonames.org/citiesJSON?north=" . $_REQUEST['north'] . "&south=" . $_REQUEST['south'] . "&east=" . $_REQUEST['east'] . "&west=" . $_REQUEST['west'] . "&lang=" . $_REQUEST['lang'] . "&username=" . $username;
+        break;
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+    default:
+        echo json_encode(['status' => ['name' => 'error', 'description' => 'Invalid action']]);
+        exit;
+}
 
-	$result=curl_exec($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
 
-	curl_close($ch);
+$result = curl_exec($ch);
+curl_close($ch);
 
-	$decode = json_decode($result,true);	
+$decode = json_decode($result, true);
 
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
-	
-	header('Content-Type: application/json; charset=UTF-8');
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode['geonames'];
 
-	echo json_encode($output); 
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($output);
 
 ?>
