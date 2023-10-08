9ac448440745c43de28e9d904e9edeb46819751c
Alerdo <alerdo23@gmail.com>
2023-10-08 21:48:30 +0100

    Add news api handling functionality

--- project1/Gazzeter/php/getNewsData.php
+++ project1/Gazzeter/php/getNewsData.php
@@ -1,23 +1,39 @@
 <?php
-
-header('Content-Type: application/json');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $apiKey = 'ac77ed5449ec4e6db6d5c234e9ac2ab2';
 $baseUrl = 'http://newsapi.org/v2/top-headlines';
 
-if(isset($_GET['countryCode'])) {
+if (isset($_GET['countryCode'])) {
     $countryCode = $_GET['countryCode'];
     
     // Construct the URL for the API request
-    $url = $baseUrl . "?country=" . $countryCode . "&apiKey=" . $apiKey;
-    
-    // Use file_get_contents to make the API call
-    $response = file_get_contents($url);
+    $url = $baseUrl . "?country=" . $countryCode;
 
-    // Return the response
-    echo $response;
-} else {
-    echo json_encode(['error' => 'No country code provided']);
+    // Initialize cURL session
+    $ch = curl_init();
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "X-Api-Key: " . $apiKey,
+        "User-Agent: MyApplicationName"  // You can replace 'MyApplicationName' with the name of your application.
+    ));
+
+    // Execute cURL session and get the response
+    $response = curl_exec($ch);
+
+    // Check for cURL errors
+    if (curl_errno($ch)) {
+        echo 'Curl error: ' . curl_error($ch);
+    } else {
+        echo $response;
+    }
+
+    // Close the cURL session
+    curl_close($ch);
 }
-
 ?>
